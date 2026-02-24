52687d3635a00a4d165daa4c3ba4a6b41d9002a7
Tassadaq Hussain <info@click4md.com>
2026-02-25 02:26:30 +0500

    Fix relation missing on Department model

--- app/Models/HR/Department.php
+++ app/Models/HR/Department.php
@@ -1,1 +1,21 @@
-<?php namespace App\Models\HR; use Illuminate\Database\Eloquent\Model; class Department extends Model { protected $table = 'hr_departments'; protected $fillable = ['clinic_id', 'name', 'description']; public function employees() { return $this->hasMany(Employee::class, 'department_id'); } }
+<?php 
+
+namespace App\Models\HR; 
+
+use Illuminate\Database\Eloquent\Model; 
+
+class Department extends Model 
+{
+    protected $table = 'hr_departments'; 
+    protected $fillable = ['clinic_id', 'name', 'description']; 
+
+    public function employees() 
+    { 
+        return $this->hasMany(Employee::class, 'department_id'); 
+    } 
+
+    public function designations()
+    {
+        return $this->hasMany(Designation::class, 'department_id');
+    }
+}
